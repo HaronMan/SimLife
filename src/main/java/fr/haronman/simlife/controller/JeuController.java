8ce39e0dbfbff629f0e7b70aadd4400a8a5293a8
HaronMan <h.kochbati59@gmail.com>
2024-07-09 19:11:48 +0200

    Premiere methode : fermer le jeu

--- src/main/java/fr/haronman/simlife/controller/JeuController.java
+++ src/main/java/fr/haronman/simlife/controller/JeuController.java
@@ -1,5 +1,10 @@
 package fr.haronman.simlife.controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
 public class JeuController {
     
+    @FXML
+    private void quitterJeu() { Platform.exit(); }
 }
